62535e362ecac7df4992f557155840e90cef70bf
abbi267 <57520740+abbi267@users.noreply.github.com>
2020-04-15 15:20:38 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,64 +1,53 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken: 'pk.eyJ1IjoiaG9qbWF4IiwiYSI6ImNrOGgxOXV3eDAydmkzbW4ycGQ2cWpiY2oifQ.UZm8Uob61yJBaRyKW6ZFsw'
-}).addTo(mymap);
-
+let coronaMap;
 let coronaDataLink = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
-function importData() {
-  maxDot = 1000000
-  minDot = 200
-  maxValue = -Infinity
-  minValue = Infinity
-  sumArray = []
-  d3.csv(coronaDataLink).then(function(data) {
-    for (let i = 0; i < data.length; i++) {
-      sum = 0
-      country = Object.values(data[i])
-      sum = parseInt(country[country.length - 1])
-      sumArray.push(sum)
-      if (sum > maxValue) {
-        maxValue = sum
+createMap()
+addCoronaData()
+
+
+function createMap() {
+  coronaMap = L.map('coronaMap').setView([30, 0], 2);                           // Lav kortet med brug af leaflet.js bibloteket
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiaG9qbWF4IiwiYSI6ImNrOGgxOXV3eDAydmkzbW4ycGQ2cWpiY2oifQ.UZm8Uob61yJBaRyKW6ZFsw'
+  }).addTo(coronaMap);
+}
+
+
+function addCoronaData() {
+  let maxDot = 400000;                                                          //Maximum størrelse på cirkler
+  let minDot = 100;                                                             //Minimum størrelse på cirkler
+  let maxValue = -Infinity;                                                     //Maximum mængde døde
+  let minValue = Infinity;                                                      //Minimum mængde døde
+  d3.csv(coronaDataLink).then(function(data) {                                  //Importer dataen med d3 bibloteket
+    for (let e of data) {                                                       //loop over alle lande, hvor et land betegnes "e"
+      country = Object.values(e)
+      countryDeath = parseInt(country[country.length - 1])                      //Den sidste værdi i listen er landets antal døde
+      if (countryDeath == 0) {                                                  //Hvis landet ikke har nogen døde, ses der bort fra landet
+        continue;
       }
-      if (sum < minValue) {
-        minValue = sum
+      if (countryDeath > maxValue) {                                            //Checker om landet har flest dødsfald i verden
+        maxValue = countryDeath
+      }
+      if (countryDeath < minValue) {                                            //Checker om landet har færrest dødsfald i verden
+        minValue = countryDeath
       }
     }
-    for (let i = 0; i < data.length; i++) {
-      val = minDot + sumArray[i] / maxValue * (maxDot - minDot)
-      L.circle([data[i]["Lat"], data[i]["Long"]], {
-        radius: minDot + sumArray[i] / maxValue * (maxDot - minDot),
-        color: 'red'
-      }).addTo(mymap);
+    for (let e of data) {                                                       //Loop over alle stykker data igen:
+      country = Object.values(e)
+      countryDeath = parseInt(country[country.length - 1])
+      if (countryDeath == 0) {                                                  //tegn IKKE cirkel, hvis der ikke er dødsfald
+        continue;
+      }
+      let size = minDot + countryDeath / maxValue * (maxDot - minDot)           //Linær transformation. Her vælges størrelsen af cirklerne på baggrund af antal døde.
+      L.circle([e["Lat"], e["Long"]], {                                         //Cirklen sættes ved koordinaterne defineret af datasættet
+        radius: size,                                                           //Sætter radius lig den beregnede værdi
+        color: 'red'                                                            //Rød farve
+      }).bindTooltip('Antal døde: ' + countryDeath).addTo(coronaMap)            //Binder et tooltip og placerer på kortet
     }
   })
 }
-
-importData()
-
-//Fjernet
-//
-// function plotCountryData(country, data) {
-//   let countryIndex = 0
-//   for (let i = 0; i < data.length; i++) {
-//     if (data[i]["Country/Region"] == country) {
-//       countryIndex = i
-//     }
-//   }
-//   let nameData = Object.getOwnPropertyNames(data[countryIndex]).splice(4, )
-//   let deathData = Object.values(data[countryIndex]).splice(4, )
-//   var data = [{
-//     x: nameData,
-//     y: deathData,
-//     type: 'bar'
-//   }];
-//   Plotly.newPlot('plotDiv', data);
-// }
-//
-
-//
