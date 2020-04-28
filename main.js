fbd6ed45964abd0148a0eb8e66995afa5e93fa3b
abbi267 <57520740+abbi267@users.noreply.github.com>
2020-04-28 09:58:38 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
 let coronaMap;
 let coronaDataLink = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
-createMap()
-addCoronaData()
+
+function setDate(days) {
+  dateLabel.html(Object.keys(data[0])[4 + slider.value()-1])
+}
+
+function changeDate() {
+  coronaMap.removeLayer(circleGroup)
+  circleGroup = L.featureGroup();
+  addCoronaData(maxSliderValue + 1 - slider.value())
+  setDate(slider.value())
+}
+
+function createTimeSlider(length) {
+  slider = createSlider(1, length, 0, 1);
+  slider.position(10, 620);
+  slider.style('width', '200px');
+  slider.input(changeDate);
+}
+
+let circleGroup = L.featureGroup();
+let slider;
+let data = undefined
+let maxSliderValue
+let dateLabel
+
+function createDateLabel() {
+  dateLabel = createP(Object.keys(data[0])[4 + slider.value()-1]);
+  dateLabel.position(10, 630)
+}
+
+function setup() {
+  createMap()
+  loadData()
+}
 
 
 function createMap() {
-  coronaMap = L.map('coronaMap').setView([30, 0], 2);                           // Lav kortet med brug af leaflet.js bibloteket
+  coronaMap = L.map('coronaMap').setView([30, 0], 2); // Lav kortet med brug af leaflet.js bibloteket
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -18,36 +50,37 @@ function createMap() {
 }
 
 
-function addCoronaData() {
-  let maxDot = 400000;                                                          //Maximum størrelse på cirkler
-  let minDot = 100;                                                             //Minimum størrelse på cirkler
-  let maxValue = -Infinity;                                                     //Maximum mængde døde
-  let minValue = Infinity;                                                      //Minimum mængde døde
-  d3.csv(coronaDataLink).then(function(data) {                                  //Importer dataen med d3 bibloteket
-    for (let e of data) {                                                       //loop over alle lande, hvor et land betegnes "e"
+function addCoronaData(daysago) {
+  if (data) {
+    for (let e of data) { //Loop over alle stykker data igen:
       country = Object.values(e)
-      countryDeath = parseInt(country[country.length - 1])                      //Den sidste værdi i listen er landets antal døde
-      if (countryDeath == 0) {                                                  //Hvis landet ikke har nogen døde, ses der bort fra landet
+      countryDeath = parseInt(country[country.length - daysago])
+      if (countryDeath == 0) { //tegn IKKE cirkel, hvis der ikke er dødsfald
         continue;
       }
-      if (countryDeath > maxValue) {                                            //Checker om landet har flest dødsfald i verden
-        maxValue = countryDeath
-      }
-      if (countryDeath < minValue) {                                            //Checker om landet har færrest dødsfald i verden
-        minValue = countryDeath
-      }
+      // let size = minDot + countryDeath / maxValue * (maxDot - minDot) //Linær transformation. Her vælges størrelsen af cirklerne på baggrund af antal døde.
+      size = Math.pow(countryDeath, 0.25) * 15000
+      L.circle([e["Lat"], e["Long"]], { //Cirklen sættes ved koordinaterne defineret af datasættet
+        radius: size, //Sætter radius lig den beregnede værdi
+        color: 'red', //Rød farve
+        weight: 5,
+      }).bindTooltip('Antal døde: ' + countryDeath).addTo(circleGroup) //Binder et tooltip og placerer på kortet
     }
-    for (let e of data) {                                                       //Loop over alle stykker data igen:
-      country = Object.values(e)
-      countryDeath = parseInt(country[country.length - 1])
-      if (countryDeath == 0) {                                                  //tegn IKKE cirkel, hvis der ikke er dødsfald
-        continue;
-      }
-      let size = minDot + countryDeath / maxValue * (maxDot - minDot)           //Linær transformation. Her vælges størrelsen af cirklerne på baggrund af antal døde.
-      L.circle([e["Lat"], e["Long"]], {                                         //Cirklen sættes ved koordinaterne defineret af datasættet
-        radius: size,                                                           //Sætter radius lig den beregnede værdi
-        color: 'red'                                                            //Rød farve
-      }).bindTooltip('Antal døde: ' + countryDeath).addTo(coronaMap)            //Binder et tooltip og placerer på kortet
-    }
+    coronaMap.addLayer(circleGroup);
+  }
+}
+
+function loadData() {
+  d3.csv(coronaDataLink).then(function(values) {
+    data = values
+    console.log(data)
+    console.log(Object.values(data[0]))
+    console.log(Object.keys(data[0]).slice(20,101))
+    maxSliderValue = Object.values(data[0]).length - 4
+    createTimeSlider(maxSliderValue)
+    addCoronaData(maxSliderValue + 1 - slider.value())
+    createDateLabel()
   })
+
+
 }
